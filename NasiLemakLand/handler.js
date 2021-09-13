d9d1a36dd90dbafd0b0f1bf5a16cd0870e969839
EC2 Default User <ec2-user@ip-10-0-0-183.us-west-2.compute.internal>
2021-09-13 12:32:00 +0000

    Added order SAM templates

---
+++ NasiLemakLand/handler.js
@@ -0,0 +1,6 @@
+
+exports.nasiLemakLand = async (event) => {
+    console.log('Nasi Lemak Land recieved an order');
+    console.log(event);
+    return;
+}
---
+++ PrimeBurgers/handler.js
@@ -0,0 +1,6 @@
+
+exports.primeBurgers = async (event) => {
+    console.log('Prime Burgers recieved an order');
+    console.log(event);
+    return;
+}
